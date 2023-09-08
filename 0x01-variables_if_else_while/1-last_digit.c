1be316b00d988cddc37af07aa5abc91c31e21c0c
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-09-08 12:09:03 -0700

    printing random values with if, else if, else in C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,9 +11,28 @@
 int main(void)
 {
 	int n;
+	int mystery;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	mystery = n % 10;
 
-	if 
+	if (mystery >= 6)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, mystery);
+	}
+	else if (mystery > 0 && mystery <= 5)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 6\n", n, mystery);
+	}
+	else if (mystery < 0)
+	{
+		printf("Last digit of %d id %d and is less then 6 and not 6\n", n, mystery);
+	}
+	else
+	{
+		printf("Last digit of %d id %d and is 0\n", n, mystery);
+	}
+
+	return (0);
 }
