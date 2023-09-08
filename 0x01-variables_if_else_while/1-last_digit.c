2ab06351aaed45e664c66c23ed57ba70c67a07c9
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-09-08 11:18:47 -0700

    variable_if,else,while and for

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - printing the value of positive, negative and zeor with string
+ *
+ * Return: Always 0 is success
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if 
+}
