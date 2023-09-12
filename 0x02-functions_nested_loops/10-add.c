1f6bb60bb82b5973d5b54f888e6cb4ba4e0db66a
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-09-12 15:28:58 -0700

    adding two values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers together
+ * @addX: add y value
+ * @addY: add x value
+ * Return: success or error
+ */
+
+int add(int addX, int addY)
+{
+	return (addX + addY);
+}
