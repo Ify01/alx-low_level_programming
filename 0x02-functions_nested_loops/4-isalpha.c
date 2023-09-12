c9ae811998fefbd870b02cdd207c2496ca460255
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-09-12 12:56:06 -0700

    printing c in upperCase and returning 1

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - prints upper case.
+ * @c: initialze c
+ * Return: return successfully.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
