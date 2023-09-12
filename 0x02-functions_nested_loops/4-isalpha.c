20d85df248bbb08722754a75616929a09dcd9d90
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-09-12 13:02:01 -0700

    printing c in upperCase and returning 1

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -8,12 +8,12 @@
 
 int _isalpha(int c)
 {
-	if (c >= 'A' && c <= 'Z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-		return (0);
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
