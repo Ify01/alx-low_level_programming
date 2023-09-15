27ce7f382bc8aa029979f1b697ff7d2f55784ea9
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-09-14 22:25:58 -0700

    printing number 0 - 14, 10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - printing 10times digits
+ * Return: success or error
+ */
+
+void more_numbers(void)
+{
+
+	int count;
+	int values;
+
+	for (count = 0; count <= 10; count++)
+	{
+		for (values = 0; values <= 14; values++)
+		{
+			if (values >= 10)
+			{
+				_putchar((values / 10) + '0');
+			}
+			_putchar((values % 10) + '0');
+		}
+	_putchar('\n');
+	}
+}
