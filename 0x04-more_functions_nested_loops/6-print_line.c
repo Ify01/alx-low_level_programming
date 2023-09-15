2db5ff4f54b9855e1be66bc3de8866b31fa050e4
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-09-14 23:04:39 -0700

    dash printing

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,17 +10,17 @@ void print_line(int n)
 {
 
 	if (n <= 0)
-	
+
 		_putchar('\n');
-	
+
 	else
 	{
 		int dash;
 
 		for (dash = 1; dash <= n; dash++)
-		
+
 			_putchar('_');
 		_putchar('\n');
-		
+
 	}
 }
