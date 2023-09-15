14e31eb977af66590e903141d97e2050d7d4bca6
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-09-15 13:13:47 -0700

    printing the size of square and length of size

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - hashtag(#) printing
+ * @n: dash
+ * Return: success or error
+ */
+
+void print_square(int size)
+{
+	int sizeOfSquare;
+	int lengthOfSquare;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (sizeOfSquare = 1; sizeOfSquare <= size; sizeOfSquare++)
+		{
+			for (lengthOfSquare = 1; lengthOfSquare <= size; lengthOfSquare++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
+
