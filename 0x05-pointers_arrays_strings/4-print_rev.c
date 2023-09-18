9e608beb59ec5283d517cbf6295d814fac096a8d
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-09-18 13:32:54 -0700

    printing the quote of Issac Asimov in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+ * print_rev - printing string in reberse form
+ * @s: the string of the function
+ * Return: Always 0.
+ */
+
+void print_rev(char *s)
+{
+	int reverseStringOrder = strlen(s);
+	int stringLetter;
+
+	for (stringLetter = reverseStringOrder - 1; stringLetter >= 0; stringLetter--)
+	{
+		_putchar(s[stringLetters]);
+	}
+	_putchar('\n');
+}
+
