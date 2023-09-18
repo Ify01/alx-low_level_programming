e612a283d45fb94d17e9e8542e841999e60c7377
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-09-18 16:37:50 -0700

    printing array in orderly form

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - printing string in half when the total is zero
+ * @a: array
+ * @n: elements of arrays to be printed
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int orderlyArray = 0;
+
+	while (orderlyArray < n)
+	{
+		if (orderlyArray > 0)
+		{
+			printf(", ");
+		}
+		printf("%d", arr[orderlyArray]);
+		orderlyArray++;
+	}
+	printf("\n");
+}
