bb18741e304d6d2b57c9b9096fb98ef417e7dae6
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-09-25 14:22:51 -0700

    a function that prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_chessboard - a function that prints the chessboard.
+ * @board: chessboard
+ * Return: success or error
+ */
+
+
+void print_chessboard(char board[8][8])
+{
+	int chess;
+	int boardChess;
+
+	for (chess = 0; chess < 8; chess++)
+	{
+		for (boardChess = 0; boardChess < 8; boardChess++)
+		{
+			_putchar(board[chess][boardChess]);
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
