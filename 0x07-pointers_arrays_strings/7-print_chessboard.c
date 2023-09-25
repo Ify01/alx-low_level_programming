8208ab820688fc2027ee37d1a50c57380537d64d
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-09-25 14:25:16 -0700

    a function that prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -17,7 +17,6 @@ void print_chessboard(char board[8][8])
 		for (boardChess = 0; boardChess < 8; boardChess++)
 		{
 			_putchar(board[chess][boardChess]);
-			_putchar(' ');
 		}
 		_putchar('\n');
 	}
