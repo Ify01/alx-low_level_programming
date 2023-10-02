3e94fbcd22a7a2e12594951459853a0918e65601
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-10-02 15:49:32 -0700

    atoi

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,1 +1,40 @@
 #include "main.h"
+
+/**
+ * _atoi - check the code
+ * @s: the string
+ * Return: Always 0.
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1;
+    int result = 0;
+    int i = 0;
+
+    while (s[i] == ' ' || (s[i] >= 9 && s[i] <= 13)) {
+        i++;
+    }
+
+    if (s[i] == '-' || s[i] == '+') {
+        if (s[i] == '-') {
+            sign = -1;
+        }
+        i++;
+    }
+
+    while (s[i] >= '0' && s[i] <= '9') {
+        if (result > (2147483647 - (s[i] - '0')) / 10) {
+            if (sign == 1) {
+                return (2147483647);
+            } else {
+                return (-2147483648);
+            }
+        }
+
+        result = result * 10 + (s[i] - '0');
+        i++;
+    }
+
+    return (result * sign);
+}
