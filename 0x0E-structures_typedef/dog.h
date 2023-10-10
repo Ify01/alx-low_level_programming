6b5a92a9201afa6a7d5c89ba823519d0734a6623
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-10-10 07:57:14 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,14 +8,13 @@
  * @owner: dog owner
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
-typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
