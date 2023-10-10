12429a9331759ef1ff31e6bf798f0904d52c8910
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-10-10 04:34:54 -0700

    a new type struct dog with the following elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a new type struct dog with the following elements
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
 struct dog
 {
 	char *name;
