8e66175cefdc26c42b32d226e7c3e4054ae5e77d
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-10-19 07:37:52 -0700

    prints

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,10 +5,10 @@
  * Return: success or error
  */
 
-void before_main_message()
+void theFunction() __attribute__((constructor));
+
+void theFunction()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-
-	return;
 }
