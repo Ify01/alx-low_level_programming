f44a0518efcdbf01f7ca833b6b26ce34e58af666
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-12-07 16:11:24 -0800

    add

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - a function that adds a new node at the beginning
+ * add_dnodeint_end - a function that adds a new node at the end
+ *
  * @head: head
  * @n: value
  * Return: success or error
