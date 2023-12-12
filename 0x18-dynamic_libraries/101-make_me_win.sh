3c78c1ddbbaf72b10f95ea0c1eed6c7b4a36a39f
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-12-11 18:26:39 -0800

    win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-Wget -P .. https://raw.githubusercontent.com/Ify01/alx-low_level_programming/master/0x18-dynamic_libraries/libwin.so
-export LD_PRELOAD="$PWD/../libwin.so"
+Wget -P .. https://raw.githubusercontent.com/Ify01/alx-low_level_programming/master/0x18-dynamic_libraries/libwinning.so
+export LD_PRELOAD="$PWD/../libwinning.so"
