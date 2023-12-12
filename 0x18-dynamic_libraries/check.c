3c1ef56ae49334b9a3a8168018e050029dfd8735
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-12-11 17:13:14 -0800

    dynamics

---
+++ 0x18-dynamic_libraries/check.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * add - adding
+ * @a: value
+ * @b: value
+ * Return: success or error
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * minus - substracting
+ * @a: value
+ * @b: value
+ * Return: success or error
+ */
+
+int minus(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * divide - adding
+ * @a: value
+ * @b: value
+ * Return: success or error
+ */
+
+int divide(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * times - multiplication
+ * @a: value
+ * @b: value
+ * Return: success or error
+ */
+
+int times(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * remainder - remaining
+ * @a: value
+ * @b: value
+ * Return: success or error
+ */
+
+int reminder(int a, int b)
+{
+	return (a % b);
+}
